efa9aed58524da39a993a1c27913b461806b8bb4
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-02-20 16:55:37 -0500

    updates

--- rover.js
+++ rover.js
@@ -1,18 +1,38 @@
 class Rover {
    // Write code here!
+   constructor(position, mode = 'NORMAL', generatorWatts = 110){
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+    }
+    receiveMessage(message){
+      let roverCommands = {
+      message : message.name,
+      results : []
+      }
+      for(let i = 0; i < message.commands.length; i++){
+         if (message.commands[i] == 'MOVE'){
+            if (this.mode == 'LOW_POWER'){
+               result.push('completed: false')
+            }
+         }
+      }
+      return results
+    }
+    
 }
 
 module.exports = Rover;
 
 let rover = new Rover(100);
-let commands = [
-   new Command('MOVE', 4321),
-   new Command('STATUS_CHECK'),
-   new Command('MODE_CHANGE', 'LOW_POWER'),
-   new Command('MOVE', 3579),
-   new Command('STATUS_CHECK')
-];
-let message = new Message('TA power', commands);
+// let commands = [
+//    new Command('MOVE', 4321),
+//    new Command('STATUS_CHECK'),
+//    new Command('MODE_CHANGE', 'LOW_POWER'),
+//    new Command('MOVE', 3579),
+//    new Command('STATUS_CHECK')
+// ];
+// let message = new Message('TA power', commands);
 let response = rover.receiveMessage(message);
 
 console.log(JSON.stringify(response, null, 2));
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -9,5 +9,28 @@ const Command = require('../command.js');
 describe("Rover class", function() {
 
   // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function(){
+    let testRover = new Rover(1200)
+    expect(testRover).toEqual({position: 1200, mode: 'NORMAL', generatorWatts: 110})
+  })
+  it("response returned by receiveMessage contains the name of the message", function(){
+    let response = rover.receiveMessage(message);
+    expect(response.message).toEqual(respone.message)
+  })
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function(){
+    
+  })
+  it("responds correctly to the status check command", function(){
+    
+  })
+  it("responds correctly to the mode change command", function(){
+    
+  })
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function(){
+    
+  })
+  it("responds with the position for the move command", function(){
+    
+  })
 
 });
