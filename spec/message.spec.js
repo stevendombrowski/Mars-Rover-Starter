de185aa22e232062042ef66d733a7eb079a51a22
Steven <dombrowskis153@yahoo.com>
2024-02-19 22:30:57 -0500

    task 3 done

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,15 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
-
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Name required.'));
+      });
+      it("constructor sets name", function(){
+        let testCommand = new Message('Test message with two commands', 'commands');
+        expect(testCommand.name).toEqual('Test message with two commands')
+      });
+      it("contains a commands array passed into the constructor as the 2nd argument ", function(){
+        let testCommand = new Message('Test message with two commands', 'commands');
+        expect(testCommand.commands).toEqual('commands')
+      })
 });
