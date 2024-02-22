b40196d29e34d0c67db140d750e5be3f2578845a
Steven Dombrowski <dombrowskis153@yahoo.com>
2024-02-22 17:04:58 -0500

    Completed MOVE Command with LowPower

--- command.js
+++ command.js
@@ -10,6 +10,5 @@ class Command {
  }
  
 
- let testCommand = new Command('MODE_CHANGE', 'LOW_POWER');
- console.log(testCommand.value)
+ 
  module.exports = Command;
--- rover.js
+++ rover.js
@@ -1,3 +1,6 @@
+const Message = require('./message.js');
+const Command = require('./command.js');
+
 class Rover {
    // Write code here!
    constructor(position, mode = 'NORMAL', generatorWatts = 110){
@@ -5,34 +8,54 @@ class Rover {
       this.mode = mode;
       this.generatorWatts = generatorWatts;
     }
-    receiveMessage(message){
-      let roverCommands = {
-      message : message.name,
-      results : []
-      }
-      for(let i = 0; i < message.commands.length; i++){
-         if (message.commands[i] == 'MOVE'){
-            if (this.mode == 'LOW_POWER'){
-               result.push('completed: false')
-            }
-         }
-      }
-      return results
-    }
     
-}
+    receiveMessage(message){
+      let commands = message.commands
+      let receivedMessages = message
+      let result = {
+         receivedMessages,
+         results : [],
 
+      }
+      for (let i = 0; i < commands.length; i++){
+         if (commands[i].commandType === 'MOVE'){
+            if (this.mode === 'LOW_POWER'){
+               result.results.push("completed: false")
+            }else{
+            this.position = commands[i].value}}
+         else if (commands[i].commandType === 'MODE_CHANGE'){
+            this.mode = commands[i].value
+         }
+         else if (commands[i].commandType === 'STATUS_CHECK'){
+            result.results.push( {
+               receivedMessage :`${message.name} received!`,
+               roverStatus : {
+                  mode: this.mode,
+                  position : this.position,
+                  generatorWatts : this.generatorWatts
+               }
+            }
+         )}
+      }
+      console.log(result.results)
+      
+
+}
+}
 module.exports = Rover;
 
+
+
 let rover = new Rover(100);
-// let commands = [
-//    new Command('MOVE', 4321),
-//    new Command('STATUS_CHECK'),
-//    new Command('MODE_CHANGE', 'LOW_POWER'),
-//    new Command('MOVE', 3579),
-//    new Command('STATUS_CHECK')
-// ];
-// let message = new Message('TA power', commands);
+let commands = [
+   new Command('MOVE', 4321),
+   new Command('STATUS_CHECK'),
+   new Command('MODE_CHANGE', 'LOW_POWER'),
+   new Command('MOVE', 3579),
+   new Command('STATUS_CHECK')
+];
+let message = new Message('TA power', commands);
 let response = rover.receiveMessage(message);
 
-console.log(JSON.stringify(response, null, 2));
+
+// console.log(JSON.stringify(response, null, 2));
