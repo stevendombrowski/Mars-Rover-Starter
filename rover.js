d1ac037490af358c733e430975aef647e87f10be
Steven <dombrowskis153@yahoo.com>
2024-02-23 00:37:10 -0500

    Returning [Object] fixed

--- rover.js
+++ rover.js
@@ -1,6 +1,5 @@
 const Message = require('./message.js');
 const Command = require('./command.js');
-
 class Rover {
    // Write code here!
    constructor(position, mode = 'NORMAL', generatorWatts = 110){
@@ -11,9 +10,8 @@ class Rover {
     
     receiveMessage(message){
       let commands = message.commands
-      let receivedMessages = message
       let result = {
-         receivedMessages,
+         receivedMessage :`${message.name} received!`,
          results : [],
 
       }
@@ -22,13 +20,14 @@ class Rover {
             if (this.mode === 'LOW_POWER'){
                result.results.push("completed: false")
             }else{
-            this.position = commands[i].value}}
+            this.position = commands[i].value,
+            result.results.push("completed: true")}}
          else if (commands[i].commandType === 'MODE_CHANGE'){
             this.mode = commands[i].value
+            result.results.push("completed: true")
          }
          else if (commands[i].commandType === 'STATUS_CHECK'){
             result.results.push( {
-               receivedMessage :`${message.name} received!`,
                roverStatus : {
                   mode: this.mode,
                   position : this.position,
@@ -37,7 +36,7 @@ class Rover {
             }
          )}
       }
-      console.log(result.results)
+      return result
       
 
 }
@@ -57,5 +56,4 @@ let commands = [
 let message = new Message('TA power', commands);
 let response = rover.receiveMessage(message);
 
-
-// console.log(JSON.stringify(response, null, 2));
+console.log(JSON.stringify(response, null, 2));
